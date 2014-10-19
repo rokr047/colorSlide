599045f83c47de194b3c73c4e62ca95e5366bdc6
Karthikeyan Sreenivasan <rokr047@gmail.com>
2014-10-19 00:50:04 -0700

    Font & SwipeManager
    Added new font files Hallo Sans and Swipe manager script to get swipe
    data.

---
+++ Assets/Script/SwipeManager.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeManager : MonoBehaviour 
+{
+	public enum Swipe
+	{
+		None,
+		Up,
+		Down,
+		Left,
+		Right
+	};
+
+	public float minSwipeLength = 100.0f;
+	public float scaleValue;
+
+	float SPEEDMULTIPLIER = 10.0f;
+
+	Vector2 swipeStartPos;
+	Vector2 swipeEndPos;
+	Vector2 swipeVector;
+
+	GameObject respawnObj;
+
+	//GameManager gManager;
+
+	public static Swipe swipeDirection;
+
+	void Start () 
+	{
+		swipeDirection = Swipe.None;
+
+		//gManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+	}
+
+	void Update () 
+	{
+		#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+		DetectSwipeFromMouse();
+		#else
+		DetectSwipeFromTouch();
+		#endif
+	}
+
+	void DetectSwipeFromTouch()
+	{
+		if (Input.touches.Length > 0) 
+		{
+			Touch t = Input.GetTouch (0);
+
+			if (t.phase == TouchPhase.Began) 
+			{
+				swipeStartPos = new Vector2 (t.position.x, t.position.y);
+
+				if((respawnObj = GameObject.FindGameObjectWithTag("Respawn")) != null)
+					respawnObj.transform.localScale = new Vector3(GameObject.FindGameObjectWithTag("Respawn").transform.localScale.x * scaleValue, GameObject.FindGameObjectWithTag("Respawn").transform.localScale.y * scaleValue, GameObject.FindGameObjectWithTag("Respawn").transform.localScale.z * scaleValue);
+			}
+
+			if(t.phase == TouchPhase.Ended)
+			{
+				swipeEndPos = new Vector2(t.position.x,t.position.y);
+
+				if((respawnObj = GameObject.FindGameObjectWithTag("Respawn")) != null)
+					respawnObj.transform.localScale = new Vector3(GameObject.FindGameObjectWithTag("Respawn").transform.localScale.x / scaleValue, GameObject.FindGameObjectWithTag("Respawn").transform.localScale.y / scaleValue, GameObject.FindGameObjectWithTag("Respawn").transform.localScale.z / scaleValue);
+
+				ComputeSwipeDirection();
+			}
+		} 
+		else 
+		{
+			swipeDirection = Swipe.None;
+		}
+	}
+
+	void DetectSwipeFromMouse()
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			swipeStartPos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+
+			if((respawnObj = GameObject.FindGameObjectWithTag("Respawn")) != null)
+				respawnObj.transform.localScale = new Vector3(GameObject.FindGameObjectWithTag("Respawn").transform.localScale.x * scaleValue, GameObject.FindGameObjectWithTag("Respawn").transform.localScale.y * scaleValue, GameObject.FindGameObjectWithTag("Respawn").transform.localScale.z * scaleValue);
+		}
+
+		if (Input.GetMouseButtonUp (0)) 
+		{
+			swipeEndPos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+
+			if((respawnObj = GameObject.FindGameObjectWithTag("Respawn")) != null)
+				respawnObj.transform.localScale = new Vector3(GameObject.FindGameObjectWithTag("Respawn").transform.localScale.x / scaleValue, GameObject.FindGameObjectWithTag("Respawn").transform.localScale.y / scaleValue, GameObject.FindGameObjectWithTag("Respawn").transform.localScale.z / scaleValue);
+
+			ComputeSwipeDirection();
+		}
+	}
+
+	void ComputeSwipeDirection()
+	{
+		swipeVector = swipeEndPos - swipeStartPos;
+		
+		if(swipeVector.sqrMagnitude < minSwipeLength * minSwipeLength)
+		{
+			//We will not consider this as swipe.
+			swipeDirection = Swipe.None;
+			return;
+		}
+
+		#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+		//Do not change the value of cube movespeed.
+		#else
+		//gManager.cubeMoveSpeed = swipeVector.magnitude * SPEEDMULTIPLIER;
+		#endif
+
+		swipeVector.Normalize();
+
+		if (swipeVector.y > 0 &&  swipeVector.x > -0.5f && swipeVector.x < 0.5f) 
+		{
+			swipeDirection = Swipe.Up;
+		} 
+		else if (swipeVector.y < 0 && swipeVector.x > -0.5f && swipeVector.x < 0.5f) 
+		{
+			swipeDirection = Swipe.Down;
+		} 
+		else if (swipeVector.x < 0 && swipeVector.y > -0.5f && swipeVector.y < 0.5f) 
+		{
+			swipeDirection = Swipe.Left;
+		} 
+		else if (swipeVector.x > 0 && swipeVector.y > -0.5f && swipeVector.y < 0.5f) 
+		{
+			swipeDirection = Swipe.Right;
+		}
+	}
+}
